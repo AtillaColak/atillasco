b490d3af543b3a49805156db81ea0ba64274fa02
Atilla Colak <atilla.colak@outlook.com>
2024-09-01 01:20:25 +0200

    new book report

--- components/book-component.tsx
+++ components/book-component.tsx
@@ -12,10 +12,10 @@ const getTagColor = (tag: string): string => {
         tech: "bg-blue-200 text-blue-800",
         economics: "bg-green-200 text-green-800",
         history: "bg-purple-200 text-purple-800",
-        Finance: "bg-yellow-200 text-yellow-800",
-        Statistics: "bg-pink-200 text-pink-800",
-        Politics: "bg-teal-200 text-teal-800",
-        // TODO: Add more tag-color mappings
+        business: "bg-yellow-200 text-yellow-800",
+        marketing: "bg-pink-200 text-pink-800",
+        psychology: "bg-teal-200 text-teal-800",
+        // TODO: Add more tag-color mappings -> POLITICS, FINANCE, STATISTICS
     };
     return tagColors[tag] || "bg-gray-200 text-gray-800"; // Default color if no match is found
 };
