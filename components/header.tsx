15c24a04855225530acf74dcd61c89de219118cd
Atilla Colak <atilla.colak@outlook.com>
2024-09-05 01:59:09 +0200

    untested UI update - ROULETTE

--- components/header.tsx
+++ components/header.tsx
@@ -26,21 +26,26 @@ const HeaderNavBar = () => {
         <header className="bg-black text-white p-4 flex justify-between items-center shadow-lg fixed top-0 w-full z-50">
             <div className="flex items-center space-x-4">
                 <button
-                    className="transition ease-in-out duration-300 hover:text-hs-third text-hs-base text-l hover:scale-125 ml-4"
-                    onClick={handleHome}
+                    className="transition ease-in-out duration-300 hover:text-hs-third text-hs-base text-sm md:text-l hover:scale-125 ml-4"
+            onClick={handleHome}
                 >
                     Are You Not Entertained?
                 </button>
             </div>
             <nav className="flex items-center space-x-4">
                 <button
-                    className="transition ease-in-out duration-300 bg-hs-base text-black px-4 py-2 rounded-lg hover:bg-hs-secondary hover:text-hs-third"
+                    className="transition ease-in-out duration-300 bg-hs-base text-black px-4 py-2 rounded-lg hover:bg-hs-secondary hover:text-hs-third text-sm md:text-base"
                     onClick={handleYouTube}
                 >
                     Video of the Week
                 </button>
 
-                <button className="transition ease-in-out duration-300 hover:text-hs-base focus:outline-none" onClick={handleAboutUs}>Books I Read</button>
+                <button   
+                    className="transition ease-in-out duration-300 hover:text-hs-base focus:outline-none text-sm md:text-base"
+                    onClick={handleAboutUs}
+                >
+                    Books I Read
+                </button>
             </nav>
         </header>
     );
